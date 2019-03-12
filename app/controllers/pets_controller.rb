414b9847dc31e8972cbe645c2087397731ac36c7
Samantha brake <sammy.brake@gmail.com>
2019-03-12 12:53:45 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -43,6 +43,7 @@ class PetsController < ApplicationController
     # binding.pry
  
     @pet = Pet.find(params[:id])
+    binding.pry
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params[:owner][:name])
